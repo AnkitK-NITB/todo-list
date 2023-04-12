a57c5a5aa29fd443c305b278b9044f83ba6965cf
AnkitK-NITB <>
2023-04-13 03:05:23 +0530

    Add mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,29 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+
 const listsroutes = require('./routes/lists')
 
 const app = express()
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
         console.log(req.path, req.method)
         next()
 })
 
 // routes
-app.use('/api/lists',listsroutes)
+app.use('/api/lists', listsroutes)
 
-// listen for requests
-app.listen(process.env.PORT, ()=>{
-        console.log("Listening on PORT:", process.env.PORT)
+//connnect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+        
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+                console.log("Connected to MongoDB & Listening on PORT:", process.env.PORT)
+        })
 })
+.catch((err) => {
+                console.log(process.env.MONGO_URI)
+                console.log(err)
+        })
