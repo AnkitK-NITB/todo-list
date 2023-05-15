a70e9535ae171c38d82e103f75d5ae1224b5e9ae
AnkitK-NITB <>
2023-05-15 17:25:02 +0530

    improve css

--- frontend/src/components/ItemDetails.js
+++ frontend/src/components/ItemDetails.js
@@ -1,6 +1,6 @@
 const  ItemDetails = ({item})=>{
     return (
-        <div className="item-detail">
+        <div className="item">
             <h4>{item.title}</h4>
             <p>{item.description}</p>
             <p>{item.createdAt}</p>
